1ffe392f7eeeb5d796882569b57d1f0864a8b1c8
Sr. Mies <ignaciomies@live.com.ar>
2018-09-10 17:14:00 -0300

    Parte 1: COMPLETA

--- Workshop1/workshop.js
+++ Workshop1/workshop.js
@@ -1,4 +1,5 @@
 
+loadStudents();
 function validateField(event) {
     var lementId = event.target.id;
     var formNode = document.getElementById(lementId);
@@ -8,6 +9,10 @@ function validateField(event) {
     } else {
         formNode.classList.add('is-valid');
         formNode.classList.remove('incomplete');
+        var invalids = document.getElementsByClassName('incomplete');
+        if(invalids.length === 0) {
+            document.getElementById('btnAddStudent').disabled = false;
+        }
     }
 }
 function resetValidator(event) {
@@ -17,11 +22,6 @@ function resetValidator(event) {
     formNode.classList.remove('is-valid');
 }
 function addStudent() {
-    var invalids = document.getElementsByClassName('incomplete');
-    if(invalids.length != 0) {
-        alert('Debe un nombre y DNI válidos.');
-        return;
-    }
     if(txtDni.value < 0) {
         alert('DNI inválido.');
         return;
@@ -33,6 +33,21 @@ function addStudent() {
     var student = new Student(txtFirstName.value, txtLastName.value, txtDni.value, txtEmail.value);
     var studentJSON = JSON.stringify(student);
     localStorage.setItem(student.dni, studentJSON);
+    var ulNode = document.getElementById('mainList'); //Container principal
+    var liNode = document.createElement('li');  //fila de la lista
+    liNode.id = student.dni;
+    liNode.className = 'list-group-item';
+    var h1Node = document.createElement('h1');
+    h1Node.innerHTML = student.firstName + ' ' + student.lastName
+    var h3Node = document.createElement('h3');
+    h3Node.innerHTML = 'DNI: ' + student.dni;
+    liNode.appendChild(h1Node);
+    liNode.appendChild(h3Node);
+    ulNode.appendChild(liNode);
+    txtDni.value = '';
+    txtFirstName.value = '';
+    txtLastName.value = '';
+    txtEmail.value = '';
 }
 function Student(firstName, lastName, dni, email) {
     this.firstName = firstName;
@@ -49,4 +64,26 @@ function deleteStudent() {
         return;
     }
     localStorage.removeItem(txtDniDelete.value);
+    var studentToDelete = document.getElementById(txtDniDelete.value)
+    mainList.removeChild(studentToDelete);
+    txtDniDelete.value = '';
+}
+
+function loadStudents(){
+    for(a = 0; a < localStorage.length; a++){
+        var key = localStorage.key(a);
+        var item = localStorage.getItem(key);
+        var student = JSON.parse(item);
+        var ulNode = document.getElementById('mainList'); //Container principal
+        var liNode = document.createElement('li');  //fila de la lista
+        liNode.id = student.dni;
+        liNode.className = 'list-group-item';
+        var h1Node = document.createElement('h1');
+        h1Node.innerHTML = student.firstName + ' ' + student.lastName
+        var h3Node = document.createElement('h3');
+        h3Node.innerHTML = 'DNI: ' + student.dni;
+        liNode.appendChild(h1Node);
+        liNode.appendChild(h3Node);
+        ulNode.appendChild(liNode);
+    }
 }
