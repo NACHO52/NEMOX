a5d253a41d879f19cf409071ee82ed9bbaf88d11
Nacho <ignaciomies@live.com>
2018-09-21 22:24:47 -0300

    asd

--- Workshop1/workshop.js
+++ Workshop1/workshop.js
@@ -1,17 +1,52 @@
-
+createList();
 loadStudents();
+function createList(){
+    var existing = localStorage.getItem('StudentList');
+    if(existing) {
+        return;
+    } else {
+        var students = [];
+        var studentJSON = JSON.stringify(students);
+        localStorage.setItem('StudentList', studentJSON);
+    }
+}
+function loadStudents(){
+    var obj = localStorage.getItem('StudentList');
+    var alumno = JSON.parse(obj);
+    for(a = 0; a < alumno.length; a++){
+        var item =  alumno[a];
+        createNewElement(item);
+    }
+}
+function createNewElement(item) {
+    var ulNode = document.getElementById('mainList');
+    var liNode = document.createElement('li');
+    liNode.id = 'saved-' + item.dni;
+    liNode.className = 'list-group-item';
+    var h1Node = document.createElement('h1');
+    h1Node.innerHTML = item.firstName + ' ' + item.lastName;
+    var h3Node = document.createElement('h3');
+    h3Node.innerHTML = 'DNI: ' + item.dni;
+    var h4Node = document.createElement('h4');
+    h4Node.innerHTML = 'E-mail: ' + item.email;
+    liNode.appendChild(h1Node);
+    liNode.appendChild(h3Node);
+    liNode.appendChild(h4Node);
+    ulNode.appendChild(liNode);
+}
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
 function validateField(event) {
     var elementId = event.target.id;
     var formNode = document.getElementById(elementId);
-    if(formNode.id === 'txtEmail' && (formNode.value.indexOf('@') === -1 || formNode.value.indexOf('.') === -1)) {
+    if((formNode.id === 'txtEmail' && validateEmail(txtEmail.value) === false) || formNode.value === '' || (formNode.id === 'txtDni' && formNode.value <= 0)){
         formNode.classList.add('is-invalid');
         formNode.classList.add('incomplete');
-        return;
+        document.getElementById('btnAddStudent').disabled = true;
     }
-    if(formNode.value === '' || formNode.value == undefined || formNode.value === null) {
-        formNode.classList.add('is-invalid');
-        formNode.classList.add('incomplete');
-    } else {
+    else {
         formNode.classList.add('is-valid');
         formNode.classList.remove('incomplete');
         var invalids = document.getElementsByClassName('incomplete');
@@ -26,96 +61,84 @@ function resetValidator(event) {
     formNode.classList.remove('is-invalid');
     formNode.classList.remove('is-valid');
 }
-function addStudent() {
-    if(txtDni.value < 0) {
-        alert('DNI inválido.');
-        return;
-    }
-    if(localStorage.getItem(txtDni.value)) {
-        alert('El alumno ingresado ya existe.');
-        return;
-    }
-    var student = new Student(txtFirstName.value, txtLastName.value, txtDni.value, txtEmail.value);
-    var studentJSON = JSON.stringify(student);
-    localStorage.setItem(student.dni, studentJSON);
-    var ulNode = document.getElementById('mainList'); //Container principal
-    var liNode = document.createElement('li');  //fila de la lista
-    liNode.id = 'saved-' + student.dni;
-    liNode.className = 'list-group-item';
-    var h1Node = document.createElement('h1');
-    h1Node.innerHTML = student.firstName + ' ' + student.lastName
-    var h3Node = document.createElement('h3');
-    h3Node.innerHTML = 'DNI: ' + student.dni;
-    liNode.appendChild(h1Node);
-    liNode.appendChild(h3Node);
-    ulNode.appendChild(liNode);
-    txtDni.value = '';
-    txtFirstName.value = '';
-    txtLastName.value = '';
-    txtEmail.value = '';
-}
 function Student(firstName, lastName, dni, email) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.dni = dni;
     this.email = email;
 }
+function addStudent() {
+    if(txtDni.value < 0) {
+        alert('DNI inválido.');
+        return;
+    }
+    var obj = localStorage.getItem('StudentList');
+    var students = JSON.parse(obj);
+    var existing = students.filter(s => s.dni === txtDni.value);
+    if(existing.length != 0) {
+        alert('Los datos ingresados ya pertenecen a un alumno,\n por favor modifiquelos e intente nuevamente');
+        return;
+    }
+    var student = new Student(txtFirstName.value, txtLastName.value, txtDni.value, txtEmail.value);
+    students.push(student);
+    localStorage.setItem('StudentList', JSON.stringify(students));
+    createNewElement(student);
+    txtDni.value = '';
+    txtDni.classList.remove('is-valid');
+    txtDni.classList.remove('is-invalid');
+    txtEmail.classList.remove('is-valid');
+    txtEmail.classList.remove('is-invalid');
+    txtFirstName.classList.remove('is-valid');
+    txtFirstName.classList.remove('is-invalid');
+    txtLastName.classList.remove('is-valid');
+    txtLastName.classList.remove('is-invalid');
+    txtFirstName.value = '';
+    txtLastName.value = '';
+    txtEmail.value = '';
+    document.getElementById('btnAddStudent').disabled = true;
+}
+function checkStudent(a) {
+    return a.dni === txtDniDelete.value;
+}
 function deleteStudent() {
     if(txtDniDelete.value === '') {
         alert('Debe ingresar un número de DNI.');
         return;
-    }
-    if(!localStorage.getItem(txtDniDelete.value)) {
-        alert('El DNI ingresado no existe.');
+    } 
+    var obj = localStorage.getItem('StudentList');
+    var students = JSON.parse(obj);
+    var existing = students.filter(checkStudent);
+    if(existing.length == 0) {
+        alert('No se encontró a ningún alumno con ese DNI, porfavor verifique los datos ingresados e intente nuevamente.');
         return;
     }
-    localStorage.removeItem(txtDniDelete.value);
-    var studentToDelete = document.getElementById('saved-' + txtDniDelete.value)
-    mainList.removeChild(studentToDelete);
+    students.splice(students.findIndex(e => e.dni === txtDniDelete.value),1);
+    localStorage.setItem('StudentList', JSON.stringify(students));
     txtDniDelete.value = '';
-    alert('Se ha eliminado al alumno.');
-}
-function loadStudents(){
-    for(a = 0; a < localStorage.length; a++){
-        var key = localStorage.key(a);
-        var item = localStorage.getItem(key);
-        var student = JSON.parse(item);
-        var ulNode = document.getElementById('mainList');
-        var liNode = document.createElement('li');
-        liNode.id = 'saved-' + student.dni;
-        liNode.className = 'list-group-item';
-        var h1Node = document.createElement('h1');
-        h1Node.innerHTML = student.firstName + ' ' + student.lastName;
-        var h3Node = document.createElement('h3');
-        h3Node.innerHTML = 'DNI: ' + student.dni;
-        liNode.appendChild(h1Node);
-        liNode.appendChild(h3Node);
-        ulNode.appendChild(liNode);
-    }
+    var ulNode = document.getElementById('mainList');
+    var liNode = document.getElementById('saved-' + existing[0].dni);
+    ulNode.removeChild(liNode);
 }
 function searchStudent() {
-    var ulNode = document.getElementById('searchList');
-    ulNode.innerHTML = '';
-    if(txtSearchStudent.value === '') {
+    var name = txtSearchStudent.value.toLowerCase();
+    var ulNode = document.getElementById('mainList');
+    if(name === '') {
+        ulNode.innerHTML = '';
+        loadStudents();
         return;
     }
-    var name = txtSearchStudent.value.toLowerCase();
-    for(a = 0; a < localStorage.length; a++){
-        var key = localStorage.key(a);
-        var item = localStorage.getItem(key);
-        var student = JSON.parse(item);
-        if(student.firstName.toLowerCase().indexOf(name) != -1 || student.lastName.toLowerCase().indexOf(name) != -1) {
-            var liNode = document.createElement('li');
-            liNode.id = 'result-' + student.dni;
-            liNode.className = 'list-group-item';
-            var h1Node = document.createElement('h1');
-            h1Node.innerHTML = student.firstName + ' ' + student.lastName;
-            var h3Node = document.createElement('h3');
-            h3Node.innerHTML = 'DNI: ' + student.dni;
-            liNode.appendChild(h1Node);
-            liNode.appendChild(h3Node);
-            ulNode.appendChild(liNode);
+    ulNode.innerHTML = '';
+    var obj = localStorage.getItem('StudentList');
+    var studentList = JSON.parse(obj);
+    var matchesList = [];
+    studentList.forEach(function(item) {
+        var fullNameSpaced = item.firstName.toLowerCase() + ' ' + item.lastName.toLowerCase();
+        var fullName = item.firstName.toLowerCase() + item.lastName.toLowerCase();
+        if(fullNameSpaced.indexOf(name) != -1 || fullName.indexOf(name) != -1) {
+            matchesList.push(item);
         }
-    }
-    txtSearchStudent.value = '';
+    });
+    matchesList.forEach(function(item) {
+        createNewElement(item);
+    });
 }
